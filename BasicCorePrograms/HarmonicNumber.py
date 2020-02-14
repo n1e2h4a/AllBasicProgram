0e8dfec5cc002ac3ce1b246ebf265940e3cfa77b
niharika <guptaniharika851@gmail.com>
2020-02-14 11:59:10 +0530

    ADDED:Calculate the Harmonic Number

---
+++ BasicCorePrograms/HarmonicNumber.py
@@ -0,0 +1,12 @@
+HarmonicNumber=int(input("Enter the Number:-"))
+try:
+    if HarmonicNumber<=0:
+     print("Please Enter Valid Number")
+    else:
+        number = 1
+        for i in range(2,HarmonicNumber+1):
+            number=number+(1/i)
+        print("Nth Harmonic Number:-", (number))
+
+except:
+    print("Try it again")
