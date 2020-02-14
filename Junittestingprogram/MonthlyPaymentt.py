a4cb6b647e7d0b56ae49c24055620736ccc75217
Niharika <guptaniharika851@gmail.com>
2020-02-14 18:55:27 +0530

    ADDED:Monthly payment according to rate and time

---
+++ Junittestingprogram/MonthlyPaymentt.py
@@ -0,0 +1,13 @@
+import math
+class payment:
+    def monthly(self,principle,Year,Rate):
+
+        n = 12 * Year
+        r = Rate/(12*100)
+        Payment = principle * Rate/1-(pow(1+r, -n))
+        print(Payment)
+principle = int(input("Enter the principle:--"))
+Year = int(input("Enter the number of years:--"))
+Rate = int(input("Enter the Rate Percent:--"))
+salary=payment()
+salary.monthly(principle,Year,Rate)
