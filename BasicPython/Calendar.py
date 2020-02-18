de005fdf64973954d639a45db5ab408e3897bd8b
Niharika <guptaniharika851@gmail.com>
2020-02-18 11:32:15 +0530

    REFACTORED:provide calendar for the specified Date

---
+++ BasicPython/Calendar.py
@@ -0,0 +1,7 @@
+# import calendar module for calculating year and month
+import calendar
+# input for month and year
+Year = int(input("enter the Year:--"))
+month = int(input("enter the month:--"))
+# print the calendar for month and year
+print(calendar.month(Year, month))
