a6bff5b15b2d109b11e465b6e2f312ec8ae9458b
Niharika <guptaniharika851@gmail.com>
2020-02-18 15:42:34 +0530

    ADDED: Print the size of an object in byte

---
+++ BasicPython/sizebyte.py
@@ -0,0 +1,10 @@
+import sys
+name1 = "Neha"
+name2 = "Varun"
+name3 = "Rahul"
+name4 = "Kartik"
+print()
+print("Memory Size Of '"+name1+"' = "+str(sys.getsizeof(name1))+ " bytes")
+print("Memory size of '"+name2+"' = "+str(sys.getsizeof(name2))+ " bytes")
+print("Memory size of '"+name3+"' = "+str(sys.getsizeof(name3))+ " bytes")
+print("Memory size of '"+name4+"' = "+str(sys.getsizeof(name4))+ " bytes")
