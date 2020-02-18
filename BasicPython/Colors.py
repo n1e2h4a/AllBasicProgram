950167c4ddac6711bd67fcb94d0d3af2ddf12495
Niharika <guptaniharika851@gmail.com>
2020-02-18 11:44:52 +0530

    ADDED:print colors in sequence

---
+++ BasicPython/Colors.py
@@ -0,0 +1,9 @@
+import time
+
+colors = ["Red", "Blue", "Black", "orange"]
+# print last color from colors list
+print(f'last color is', colors[-1])
+# take time for 3 sec
+time.sleep(3)
+# print the first color of colors list
+print(f'First color is', colors[0])
