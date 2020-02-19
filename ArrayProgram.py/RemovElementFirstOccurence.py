ef389b6d8ecbb7fa846769990f79048266c04c71
Niharika <guptaniharika851@gmail.com>
2020-02-19 17:13:20 +0530

    ADDED: Remove the first occurence of a digit in list

---
+++ ArrayProgram.py/RemovElementFirstOccurence.py
@@ -0,0 +1,6 @@
+from array import*
+array_num=array('i',[1,5,7,8,7,9])
+print("Original array: "+str(array_num))
+print("Remove the first occurence of 7 from the array")
+array_num.remove(7)
+print("New Modified Array:--" +str(array_num))
