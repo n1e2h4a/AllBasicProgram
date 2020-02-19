481c782261b5b0e90433cd763980e345c7e4b358
Niharika <guptaniharika851@gmail.com>
2020-02-19 14:38:12 +0530

    ADDED: print the  element of array on index

---
+++ ArrayProgram.py/indexarrayelement.py
@@ -0,0 +1,6 @@
+Elements=[2,8,9,6,8]
+print("First element of the array at first index is:--",Elements[0])
+print("Second element of the array at first index is:--",Elements[1])
+print("Third element of the array at first index is:--",Elements[2])
+print("Fourth element of the array at first index is:--",Elements[3])
+print("Fifth element of the array at first index is:--",Elements[4])
