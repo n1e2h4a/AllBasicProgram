6d2e707847f727ba59a5ff75ee441071d11ec953
Niharika <guptaniharika851@gmail.com>
2020-02-20 10:48:27 +0530

    ADDED:Form set in different types:

---
+++ SetPrograms.py/Setform.py
@@ -0,0 +1,18 @@
+myset=set()
+print("my first set: ")
+#print Empty Set
+print(myset)
+myset1=set("pythoniseasy")
+print("\nmy set using string")
+print(myset1)
+#using object to store string
+simple="pythoniseasy"
+print("\nset with object")
+myset2 =set(simple)
+print(myset2)
+
+#create set use list
+sample=set(["python","is","easy"])
+print("\nset with use list")
+print(sample)
+
