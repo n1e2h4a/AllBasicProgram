95f720eda59d618ff576f5b7e2ba5cb9f717873b
Niharika <guptaniharika851@gmail.com>
2020-02-20 16:47:50 +0530

    ADDED:unpacking the tuble in variable

---
+++ TuplePrograms.py/unpacktuple.py
@@ -0,0 +1,7 @@
+#create a tuple
+top= 4, 8, 3
+print(top)
+n1, n2, n3 = top
+#unpack a tuple in variables
+print(n1 + n2 + n3)
+#the number of variables must be equal to the number of items of the tuple
