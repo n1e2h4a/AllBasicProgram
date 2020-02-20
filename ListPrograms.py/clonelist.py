d8e50400dac7eeee9c4b31ce4f13c9b4b088ec90
Niharika <guptaniharika851@gmail.com>
2020-02-20 14:14:56 +0530

    ADDED:clone the original list in new list and print it

---
+++ ListPrograms.py/clonelist.py
@@ -0,0 +1,8 @@
+table = []
+number = int(input("Enter number of digit you want to print:---"))
+for x in range(number):
+    Element = int((input(' > ')))
+    table.append(Element)
+new_list = list(table)
+print(table)
+print(new_list)
