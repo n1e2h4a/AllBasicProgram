86c804429a68491c0f247f1f8690de6443132a5f
Niharika <guptaniharika851@gmail.com>
2020-02-20 15:44:17 +0530

    ADDED:Elements same in both list

---
+++ ListPrograms.py/Elementsameintwolist.py
@@ -0,0 +1,15 @@
+table1 = []
+number = int(input("Enter number of digit you want to print in 1st list:---"))
+for x in range(number):
+    Element = int((input(' > ')))
+    table1.append(Element)
+print("my table1",table1)
+
+
+table2 = []
+number = int(input("Enter number of digit you want to print in 2nd list:---"))
+for x in range(number):
+    Element = int((input(' > ')))
+    table2.append(Element)
+print("my table2",table2)
+print(set(table1) & set(table2))
