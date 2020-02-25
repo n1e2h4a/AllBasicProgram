6ab8433d7ac6909401b65cc4b9d9e299072ef430
Niharika <guptaniharika851@gmail.com>
2020-02-25 11:28:09 +0530

    ADDED:convert in python list and tuple

---
+++ Numpyy/listupleinarray.py
@@ -0,0 +1,7 @@
+import numpy as np
+samplelist=[1,2,3,4,5,6,7,8]
+print("list to Array")
+print(np.asarray(samplelist))
+sampletuple=([8,4,6], [1,2,3])
+print("Tuple to Array")
+print(np.asarray(sampletuple))
