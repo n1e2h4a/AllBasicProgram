83565f2c37b3efc666625ea84da44eb96cf8af9d
Niharika <guptaniharika851@gmail.com>
2020-02-28 10:58:15 +0530

    ADDED:Deleted

--- Matplotlibprograms/MatplotBarChart/SelectWidthOfBarAndPosition.py
+++ Matplotlibprograms/MatplotBarChart/SelectWidthOfBarAndPosition.py
@@ -2,14 +2,17 @@ import matplotlib.pyplot as plt
 x = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
 popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
 x_pos = [i for i, _ in enumerate(x)]
-plt.bar(x_pos, popularity, color=(0.4, 0.6, 0.8, 1.0))
 plt.xlabel("Languages")
 plt.ylabel("Popularity")
 plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+plt.xticks(x_pos, x)
 
-plt.xticks(x_pos, x, rotation=90)
+width = [0.1,0.2,0.5,1.1,0.2,0.3]
+y_pos = [0,.8,1.5,3,5,6]
 
-plt.subplots_adjust(bottom=0.4, top=.8)
+
+plt.bar(y_pos, popularity, width=width)
+plt.xticks(y_pos, x)
 
 plt.minorticks_on()
 plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
