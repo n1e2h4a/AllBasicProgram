0bf43301631cbf97df9de12bc706994e29ff3447
Niharika <guptaniharika851@gmail.com>
2020-02-28 11:02:43 +0530

    ADDED:Python Program to plot bar chart to select width of bar and position

---
+++ Matplotlibprograms/MatplotBarChart/SpecifyPositionOfEachBar.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+x = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+x_pos = [i for i, _ in enumerate(x)]
+plt.xlabel("Languages")
+plt.ylabel("Popularity")
+plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+plt.xticks(x_pos, x)
+
+y_pos = [0,1,4,7,9,10]
+
+plt.bar(y_pos, popularity)
+
+plt.minorticks_on()
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='black')
+
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='red')
+plt.show()
