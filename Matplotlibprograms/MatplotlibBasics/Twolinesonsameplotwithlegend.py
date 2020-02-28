b39b0967ec71f2537a2ae0381ad12c638841cfc2
Niharika <guptaniharika851@gmail.com>
2020-02-28 16:21:27 +0530

    ADDED:Python program to plot two or more lines on same plot with suitable legends of each line

---
+++ Matplotlibprograms/MatplotlibBasics/Twolinesonsameplotwithlegend.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+x1=[10,20,30]
+y1=[20,40,10]
+plt.plot(x1,y1, label = "this is line first" )
+x2=[10,20,30]
+y2=[40,10,30]
+plt.plot(x2,y2, label = "this is line second")
+plt.xlabel("I am x axis")
+plt.ylabel("I am y axis")
+plt.title("Two or more lines on same plot with good legend")
+plt.legend()
+plt.show()
