ad60da6bbe3470da3214d7f7a6dd074e56847412
Niharika <guptaniharika851@gmail.com>
2020-02-28 14:29:46 +0530

    ADDED:Deleted

--- Matplotlibprograms/MatplotBasicPrograms/DrawlineofDataset1.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-df=pd.read_csv('/home/bridgelabz/Desktop/Jupyter/Dataset1.csv')
-df.plot()
-plt.show()
--- Matplotlibprograms/MatplotBasicPrograms/GivenAxisValue.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-x=[100,200,300]
-y=[200,400,600]
-plt.plot(x,y)
-plt.xlabel('Investment')
-plt.ylabel("Earning")
-plt.title("salary")
-plt.show()
